7775c0023e01286a0fa1759df21b178f637aee13
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-08-30 22:24:33 +0300

    Task47

---
+++ Task47.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine ("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double [m, n];
+for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+        matrix[i, j] = Math.Round( new Random().NextDouble() * 100, 2);
+        Console.Write(matrix[i,j]+ " ");
+    }
+    Console.WriteLine();
+}
